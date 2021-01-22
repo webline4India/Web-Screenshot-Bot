30e0b3ecd33cf5c39e0de9be233603af64cee390
webline4India <67776554+webline4India@users.noreply.github.com>
2021-01-22 17:10:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from creds import my
 plugins = dict(root="plugins")
 app = Client(
     "Webshot Bot",
-    bot_token=my.BOT_TOKEN,
-    api_id=my.API_ID,
-    api_hash=my.API_HASH,
+    bot_token=my.1573785521:AAGOyjl0Mfok6z-HNmd25X7lS3ZC82vlI1g,
+    api_id=my.2421844,
+    api_hash=my.779fe41a408add3441fd599d8e77c984,
     plugins=plugins,
 )
 
